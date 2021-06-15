9525ec6959ce8ceb06b3cc44a2daedb01a03fd09
Danielicy <danielicy@hotmail.com>
2021-06-15 12:12:40 +0300

    fixed operatorPresedence

--- calculator.api/calculator.api/Calculator/Calculator.cs
+++ calculator.api/calculator.api/Calculator/Calculator.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 
@@ -6,80 +8,111 @@ namespace calculator.api
 {   
         public class Calculator
         {
+
+        Dictionary<char, int> operatorPresedence = new Dictionary<char, int>();
+
+        public Calculator()
+        {
+            operatorPresedence['+'] = 1;
+            operatorPresedence['-'] = 1;
+            operatorPresedence['*'] = 2;
+            operatorPresedence['/'] = 2;
+        }
+
             public double Calculate(string expression)
             {
                 // Remove all spaces
                 expression = Regex.Replace(expression, @"\s+", "");
 
-                Operation operation = new Operation();
-                operation.Parse(expression);
-
-                double result = operation.Solve();
-
-                return result;
+           
+            return Solve(expression);
             }
-        }
 
-        public class Operation
+        private double Solve(string expression)
         {
-            public Operation LeftOperator { get; set; }
-            public string Operator { get; set; }
-            public Operation RightOperator { get; set; }
+            //1+8+7*4/2+5*2.5 = 9 + 14 + 12.5
 
-            private Regex additionSubtraction = new Regex("[+-]", RegexOptions.RightToLeft);
-            private Regex multiplicationDivision = new Regex("[*/]", RegexOptions.RightToLeft);
+            string buffer = "";
+           
+            Queue<string> results = new Queue<string>();
+            Stack<char> operators = new Stack<char>();
+            Stack<double> tempStack = new Stack<double>();
 
-            public void Parse(string equation)
+            foreach (char c in expression)
             {
-                var operatorLocation = additionSubtraction.Match(equation);
-                if (!operatorLocation.Success)
+                if (char.IsDigit(c) || c == '.')
+                    buffer += c;
+                else if ("+-*/".Contains(c))
                 {
-                    operatorLocation = multiplicationDivision.Match(equation);
-                }
+                    results.Enqueue(buffer);
+                    buffer = "";
 
-                if (operatorLocation.Success)
-                {
-                    Operator = operatorLocation.Value;
+                    if (operators.Count == 0)
+                        operators.Push(c);
+                    else if (operatorPresedence[c] > operatorPresedence[operators.Peek()])
+                    {
+                        operators.Push(c);
+                    }
+                    else if (operatorPresedence[c] == operatorPresedence[operators.Peek()])
+                    {
+                        results.Enqueue(operators.Pop().ToString());
+                        operators.Push(c);
+                    }
+                    else if (operatorPresedence[c] < operatorPresedence[operators.Peek()])
+                    {
+                        results.Enqueue(operators.Pop().ToString());
 
-                    LeftOperator = new Operation();
-                    LeftOperator.Parse(equation.Substring(0, operatorLocation.Index));
+                        operators.Push(c);
+                    }
 
-                    RightOperator = new Operation();
-                    RightOperator.Parse(equation.Substring(operatorLocation.Index + 1));
                 }
                 else
-                {
-                    Operator = "v";
-                    result = double.Parse(equation);
-                }
+                    throw new Exception("Invalid character in string");
+            }
+            results.Enqueue(buffer);
+
+            while (operators.Count > 0)
+            {
+                results.Enqueue(operators.Pop().ToString());
             }
 
-            private double result;
-
-            public double Solve()
+           
+            while (results.Count > 0)
             {
-                switch (Operator)
-                {
-                    case "v":
-                        break;
-                    case "+":
-                        result = LeftOperator.Solve() + RightOperator.Solve();
-                        break;
-                    case "-":
-                        result = LeftOperator.Solve() - RightOperator.Solve();
-                        break;
-                    case "*":
-                        result = LeftOperator.Solve() * RightOperator.Solve();
-                        break;
-                    case "/":
-                        result = LeftOperator.Solve() / RightOperator.Solve();
-                        break;
-                    default:
-                        throw new Exception("Call Parse first.");
-                }
+                string tmp = results.Dequeue();
 
-                return result;
+                if ("+-*/".Contains(tmp))
+                {
+                    double t1 = tempStack.Pop();
+                    double t2 = tempStack.Pop();
+                    double total = operation( t2, t1, tmp);
+                    tempStack.Push(total);
+                }
+                else
+                    tempStack.Push(double.Parse(tmp)); 
+            }
+
+            return tempStack.Pop();
+        }
+
+        private double operation(double v1, double v2, string op)
+        {
+            switch(op)
+            {
+                case "+":
+                    return v1 + v2;
+                case "-":
+                    return v1 - v2;
+                case "*":
+                    return v1 * v2;
+                case "/":
+                    return v1 / v2;
+                default:
+                    throw new Exception("Ilegal character");
+
             }
         }
+    }
+     
     
 }
--- calculator.api/calculator.test/CalculatorTest.cs
+++ calculator.api/calculator.test/CalculatorTest.cs
@@ -13,13 +13,33 @@ namespace calculator.test
         }
 
         [Test]
+        public void SimpleCalculation()
+        {
+            
+
+            Calculator _calc = new Calculator();
+            var result = _calc.Calculate("5+10*3");
+            Assert.AreEqual(35, result);
+        }
+
+        [Test]
+        public void SimpleCalculation2()
+        {
+
+
+            Calculator _calc = new Calculator();
+            var result = _calc.Calculate("8*4+2");
+            Assert.AreEqual(34, result);
+        }
+
+        [Test]
         public void CalculationOrder()
         {
             //1+8+7*4/2+5*2.5 = 9 + 14 + 12.5
 
             Calculator _calc = new Calculator();
-            var result = _calc.Calculate("1+2+3*2.9/4.6+12+2");
-            Assert.AreEqual("35.5", result);
+            var result = _calc.Calculate("1+8+7*4/2+5*2.5");
+            Assert.AreEqual(35.5, result);
         }
         [Test]
         public void CalculationOrder2()
@@ -29,18 +49,29 @@ namespace calculator.test
             Calculator _calc = new Calculator();
             var result = _calc.Calculate("1+2+3*2.9/4.6+12+2 ");
            var parsedResulr = string.Format("{0:N6}", result);
-            Assert.AreEqual("18.891304", parsedResulr);
+            Assert.AreEqual(18.891304, parsedResulr);
         }
 
         [Test]
-        public void CalculationOrder3()
+        public void NegativeResult()
         {
-            //5-2+5*7*2+12-2*3.5=3+70+12-7=
+            //5-2*5*7+2+12-2*3.5=5-70+14-7
             Calculator _calc = new Calculator();
-            var result = _calc.Calculate("5-2+5*7*2+12-2*3.5");
-            Assert.AreEqual("78", result);
+            var result = _calc.Calculate("5-2*5*7+2+12-2*3.5");
+            Assert.AreEqual(-58, result);
         }
 
+
+
+        [Test]
+        public void CalculationOrder4()
+        {
+            //5*2+5*7+2/3
+            Calculator _calc = new Calculator();
+            var result = _calc.Calculate("5*2+5*7+2/3");
+            var parsedResulr = string.Format("{0:N6}", result);
+            Assert.AreEqual("45.666667", parsedResulr);
+        }
 
 
 
